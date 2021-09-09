8fce440e010fe097aaa9ee8cdf50dfec09985448
huangchunwu <951123033@qq.com>
2021-09-10 07:32:41 +0800

    add job

--- src/main/java/cn/hcw/efund/job/FundJob.java
+++ src/main/java/cn/hcw/efund/job/FundJob.java
@@ -1,10 +1,21 @@
 package cn.hcw.efund.job;
 
+
+import cn.hcw.efund.bean.Fund;
+import cn.hcw.efund.service.FundProcessor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import us.codecraft.webmagic.Spider;
+
+
+@Service
 public class FundJob {
 
     private String url = "http://fund.eastmoney.com/004752.html";
 
-    public static void main(String[] args) {
-
+    @Scheduled(cron = "0/5 * * * * *")
+    public void run(){
+        Spider.create(new FundProcessor(new Fund())).addUrl(url).thread(1).run();
     }
+
 }
